c730099a9dddd8ae0ef58f60b6d0c0a7f7f79c5f
Martin Larralde <martin.larralde@ens-cachan.fr>
2017-07-04 00:15:33 +0200

    Add methods to _utils to check if path and handles are writable

--- fs/archive/_utils.py
+++ fs/archive/_utils.py
@@ -2,7 +2,12 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
+import os
+import io
+import sys
+import errno
 import importlib
+
 
 def import_from_names(*names):
     for name in names:
@@ -11,3 +16,27 @@ def import_from_names(*names):
         except ImportError:
             continue
     return None
+
+
+def writable_path(path):
+    if os.path.exists(path):
+        return os.access(path, os.W_OK)
+    try:
+        with open(path, 'w'):
+            pass
+    except OSError as oe:
+        return False
+    else:
+        os.remove(path)
+        return True
+
+
+def writable_stream(handle):
+    if isinstance(handle, io.IOBase) and sys.version_info >= (3, 5):
+        return handle.writable()
+    try:
+        handle.write(b'')
+    except (io.UnsupportedOperation, OSError):
+        return False
+    else:
+        return True
