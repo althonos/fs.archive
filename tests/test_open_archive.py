7083d080b421de5686140a656898407d62df10fa
Martin Larralde <martin.larralde@ens-cachan.fr>
2017-07-03 14:57:35 +0200

    Add more tests to open_archive

--- tests/test_open_archive.py
+++ tests/test_open_archive.py
@@ -2,6 +2,10 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
+import gzip
+import lzma
+import zipfile
+import tarfile
 import unittest
 
 import fs.archive
@@ -25,5 +29,72 @@ class TestOpenArchive(unittest.TestCase):
             archive.makedir('dir')
 
         with fs.archive.open_archive(mem, 'myzip.zip') as archive:
+            self.assertEqual(sorted(archive.listdir('/')), ['abc.txt', 'dir'])
+            self.assertEqual(archive.gettext('abc.txt'), 'abc')
+
+        with mem.openbin('myzip.zip') as myzip:
+            self.assertTrue(zipfile.is_zipfile(myzip))
+
+    def _test_tar(self, filename, arcfile, opener):
+
+        mem = fs.open_fs('mem://')
+
+        # Check we can write
+        with fs.archive.open_archive(mem, filename) as archive:
+            print(archive._saver.compression)
+            self.assertIsInstance(archive, fs.archive.tarfs.TarFS)
+            archive.settext('abc.txt', 'abc')
+            archive.makedir('dir')
+
+        # Check the dumped archive is a gzipped file
+        with mem.openbin(filename) as mytar:
+            arc = arcfile(fileobj=mytar)
+            arc.read()
+
+        # Check we can open it normally
+        with mem.openbin(filename) as mytar:
+            tar = getattr(tarfile.TarFile, opener)(filename, fileobj=mytar)
+        with mem.openbin(filename) as mytar:
+            tar = tarfile.TarFile.open(filename, fileobj=mytar)
+
+        # Check we can read it with the TarFS
+        with fs.archive.open_archive(mem, filename) as archive:
+            self.assertEqual(sorted(archive.listdir('/')), ['abc.txt', 'dir'])
+            self.assertEqual(archive.gettext('abc.txt'), 'abc')
+
+    def test_tar_gz(self):
+        self._test_tar('mytar.tar.gz', gzip.GzipFile, 'gzopen')
+        self._test_tar('mytar.tgz', gzip.GzipFile, 'gzopen')
+
+    def test_tar_xz(self):
+        self._test_tar('mytar.tar.xz', lzma.LZMAFile, 'xzopen')
+        self._test_tar('mytar.txz', lzma.LZMAFile, 'xzopen')
+
+
+
+    def test_tar_xz(self):
+
+        mem = fs.open_fs('mem://')
+
+        # Check we can write
+        with fs.archive.open_archive(mem, 'mytar.tar.xz') as archive:
+            print(archive._saver.compression)
+            self.assertIsInstance(archive, fs.archive.tarfs.TarFS)
+            archive.settext('abc.txt', 'abc')
+            archive.makedir('dir')
+
+        # Check the dumped archive is a gzipped file
+        with mem.openbin('mytar.tar.xz') as mytar:
+            xz = lzma.LZMAFile(mytar)
+            print(xz.read())
+
+        # Check we can open it normally
+        with mem.openbin('mytar.tar.xz') as mytar:
+            tar = tarfile.TarFile.xzopen('mytar.tar.xz', fileobj=mytar)
+        with mem.openbin('mytar.tar.xz') as mytar:
+            tar = tarfile.TarFile.open('mytar.tar.xz', fileobj=mytar)
+
+        # Check we can read it with the TarFS
+        with fs.archive.open_archive(mem, 'mytar.tar.xz') as archive:
             self.assertEqual(sorted(archive.listdir('/')), ['abc.txt', 'dir'])
             self.assertEqual(archive.gettext('abc.txt'), 'abc')
