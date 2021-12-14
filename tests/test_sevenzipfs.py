cc018c4b16bc7d7335bbfb9b493133494bb57546
Martin Larralde <martin.larralde@embl.de>
2021-12-14 14:12:01 +0100

    Skip `tests.test_sevenzipfs` if `py7zr` is not available

--- tests/test_sevenzipfs.py
+++ tests/test_sevenzipfs.py
@@ -8,31 +8,41 @@ import zipfile
 import tempfile
 import unittest
 
-import py7zr
 from six.moves import filterfalse
 
 import fs.test
 import fs.wrap
 import fs.errors
 import fs.memoryfs
-import fs.archive.sevenzipfs
 
 from fs.path import relpath, join, forcedir, abspath, recursepath
 from fs.archive.test import ArchiveReadTestCases, ArchiveIOTestCases
+
+try:
+    import py7zr
+except ImportError:
+    py7zr = None
+
+try:
+    from fs.archive.sevenzipfs import SevenZipReadFS, SevenZipFS, SevenZipSaver
+except ImportError:
+    SevenZipReadFS = SevenZipFS = SevenZipSaver = None
+
 
 
 def sevenzip_compress(handle, source_fs):
     if hasattr(handle, 'seek') and handle.seekable():
         handle.seek(0)
-    saver = fs.archive.sevenzipfs.SevenZipSaver(handle, overwrite=False)
+    saver = SevenZipSaver(handle, overwrite=False)
     saver.save(source_fs)
 
 
+@unittest.skipUnless(py7zr, 'py7zr not available')
 class TestSevenZipFS(fs.test.FSTestCases, unittest.TestCase):
 
     def make_fs(self):
         self.tempfile = tempfile.mktemp()
-        return fs.archive.sevenzipfs.SevenZipFS(self.tempfile)
+        return SevenZipFS(self.tempfile)
 
     def destroy_fs(self, fs):
         fs.close()
@@ -41,6 +51,7 @@ class TestSevenZipFS(fs.test.FSTestCases, unittest.TestCase):
         del self.tempfile
 
 
+@unittest.skipUnless(py7zr, 'py7zr not available')
 class TestSevenZipReadFS(ArchiveReadTestCases, unittest.TestCase):
 
     long_names = True
@@ -48,7 +59,7 @@ class TestSevenZipReadFS(ArchiveReadTestCases, unittest.TestCase):
 
     compress = staticmethod(sevenzip_compress)
     make_source_fs = fs.memoryfs.MemoryFS
-    _archive_read_fs = fs.archive.sevenzipfs.SevenZipReadFS
+    _archive_read_fs = SevenZipReadFS
 
     @staticmethod
     def remove_archive(handle):
@@ -59,12 +70,13 @@ class TestSevenZipReadFS(ArchiveReadTestCases, unittest.TestCase):
         super(TestSevenZipReadFS, self).setUp(handle)
 
 
+@unittest.skipUnless(py7zr, 'py7zr not available')
 class TestSevenZipFSio(ArchiveIOTestCases, unittest.TestCase):
 
     compress = staticmethod(sevenzip_compress)
     make_source_fs = fs.memoryfs.MemoryFS
-    load_archive = fs.archive.sevenzipfs.SevenZipFS
-    _archive_fs = fs.archive.sevenzipfs.SevenZipFS
+    load_archive = SevenZipFS
+    _archive_fs = SevenZipFS
 
     @staticmethod
     def iter_files(handle):
